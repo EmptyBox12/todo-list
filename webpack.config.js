21bd5a617917ddb017a3875f273db431a8f2fb05
EmptyBox12 <ataberktumay@gmail.com>
2021-09-12 15:26:44 +0300

    switched to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/index.js',
-    devtool: 'inline-source-map',
     devServer: {
         static: './dist',
     },
